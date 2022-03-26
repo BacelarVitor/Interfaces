5de2e2113128002111db018dbbfa9dc16bd3efe6
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-03-26 10:38:02 -0300

    read html from google using read interface

--- main.go
+++ main.go
@@ -1,31 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
 
-type bot interface {
-	getGreeting() string
-}
+// type bot interface {
+// 	getGreeting() string
+// }
 
-type englishBot struct{}
-type spanishBot struct{}
+// type englishBot struct{}
+// type spanishBot struct{}
 
 func main() {
-	eb := englishBot{}
-	sb := spanishBot{}
+	// eb := englishBot{}
+	// sb := spanishBot{}
 
-	PrintGreeting(eb)
-	PrintGreeting(sb)
+	// PrintGreeting(eb)
+	// PrintGreeting(sb)
+
+	resp, err := http.Get("http://google.com")
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	bs := make([]byte, 9999)
+	resp.Body.Read(bs)
+	fmt.Println(string(bs))
 }
 
-func PrintGreeting(b bot) {
-	fmt.Println(b.getGreeting())
-}
+// func PrintGreeting(b bot) {
+// 	fmt.Println(b.getGreeting())
+// }
 
-func (englishBot) getGreeting() string {
-	// VERY custom logic for generating english greeting
-	return "Hello!"
-}
+// func (englishBot) getGreeting() string {
+// 	// VERY custom logic for generating english greeting
+// 	return "Hello!"
+// }
 
-func (spanishBot) getGreeting() string {
-	return "Hola!"
-}
+// func (spanishBot) getGreeting() string {
+// 	return "Hola!"
+// }
