a8814533cb98080973b3c66270d47d3b59b773b6
Vitor Bacelar <vitor.bacelar@protonmail.com>
2022-03-26 09:25:51 -0300

    implement first interface

--- main.go
+++ main.go
@@ -1,10 +1,24 @@
 package main
 
+import "fmt"
+
+type bot interface {
+	getGreeting() string
+}
+
 type englishBot struct{}
 type spanishBot struct{}
 
 func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
 
+	PrintGreeting(eb)
+	PrintGreeting(sb)
+}
+
+func PrintGreeting(b bot) {
+	fmt.Println(b.getGreeting())
 }
 
 func (englishBot) getGreeting() string {
